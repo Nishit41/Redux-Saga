07c474324f44bfdeada0db1f1c08c29e43d9a0a4
nishit <nishitranjan41@gmail.com>
2024-09-18 06:37:43 +0530

    done with action

---
+++ src/redux/reducer.js
@@ -0,0 +1,11 @@
+import { ADD_TO_CART } from "./constant";
+
+export const cartData=(data=[],action)={
+if(action.type === ADD_TO_CART){
+   console.log("data");
+   return data
+}
+else {
+    return "no action called";
+}
+} 
