2ab47298f50e03789a354f5fb6294d39bfca6c57
nishit <nishitranjan41@gmail.com>
2024-09-18 06:23:13 +0530

    Done with action

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import {Provider} from "react-redux";
+import {Provider, useDispatch} from "react-redux";
 import Store from "./redux/store";
 
 function App() {
- <Provider store={Store}>
-     test
+  const dispatch = useDispatch();
+  const product = {
+    name: 'i Phone',
+    category: 'mobile',
+    price: 10000,
+    color: 'red'
+  }
+ return <Provider store={Store}>
+    <button onClick={()=>dispatch(product)}>test</button>
 </Provider>
 }
 
